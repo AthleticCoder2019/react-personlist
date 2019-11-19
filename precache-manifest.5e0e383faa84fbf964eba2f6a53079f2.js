46ef13a451d544399c77d0ac2a7318a266dbb690
Craig Banks Jr <cabj333@gmail.com>
2019-11-18 19:15:49 -0500

    Updates

---
+++ precache-manifest.5e0e383faa84fbf964eba2f6a53079f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6fc64472cc091f0c46f4cb03ab123ce",
+    "url": "/react-personlist/index.html"
+  },
+  {
+    "revision": "b5e6c8b16a2b9cae7b2e",
+    "url": "/react-personlist/static/css/main.1b45ee12.chunk.css"
+  },
+  {
+    "revision": "4f9f3b3a5a03a5b09f12",
+    "url": "/react-personlist/static/js/2.ccdf8ea8.chunk.js"
+  },
+  {
+    "revision": "b5e6c8b16a2b9cae7b2e",
+    "url": "/react-personlist/static/js/main.09fbc303.chunk.js"
+  },
+  {
+    "revision": "199d880a24a28afc41ee",
+    "url": "/react-personlist/static/js/runtime-main.56a89458.js"
+  }
+]);
